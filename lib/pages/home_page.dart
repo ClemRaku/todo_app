1419b8195df4d16bfd307939480cb732c595e576
Clement Raka De Costa <clement1raka@gmail.com>
2026-05-15 15:44:35 +0600

    started working on ToDo List

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        title: Text('To Do'),
+        backgroundColor: const Color.fromARGB(255, 247, 222, 5),
+      ),
+
+      body: ListView(),
+    );
+  }
+}
